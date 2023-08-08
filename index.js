8230635694662f29d3cf3d244e5bc103444aa334
chadii-t <sadiaatraore30@gmail.com>
2023-08-08 11:59:55 +0000

    faire fonctionner les boutons-pagination

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
-const section1 = document.querySelector(section1)
-const section2 = document.querySelector(section2)
-const section3 = document.querySelector(section3)
+const all = document.querySelector(".all")
+const all2 = document.querySelector(".all2")
+const all3 = document.querySelector(".all3")
+const step1 = document.querySelector(".step1")
+const step2 = document.querySelector(".step2")
+const step3 = document.querySelector(".step3")
+const btn = document.querySelector(".btn")
+const page2 = document.querySelector(".page2")
 
-function changePhoto2(fileName) {
-    let img = document.querySelector("#bannerImage");
-    img.setAttribute('src', fileName);
-    section1.style.display = 'none'
-    section2.style.display = 'block'
-    section3.style.display = 'none'
+
+window.onload = () => {
+    document.querySelector(".all").style.display = "initial";
 }
-function changePhoto1(fileName) {
-    let img = document.querySelector("#bannerImage");
-    img.setAttribute('src', fileName);
-    section1.style.display = 'none'
-    section2.style.display = 'none'
-    section3.style.display = 'block'
-}
-function changePhoto3(fileName) {
-    let img = document.querySelector("#bannerImage");
-    img.setAttribute('src', fileName);
-    section1.style.display = 'none'
-    section2.style.display = 'block'
-    section3.style.display = 'none'
-}
-function changePhoto1(fileName) {
-    let img = document.querySelector("#bannerImage");
-    img.setAttribute('src', fileName);
-    section1.style.display = 'block'
-    section2.style.display = 'none'
-    section3.style.display = 'none'
-}
+step2.addEventListener("click", () => {
+    page2.style.display = "block";
+    all.style.display = "none";
+})
+btn.addEventListener("click", () => {
+    all3.style.display = "flex";
+    all.style.display = "none";
+})
+
+
+
+
+// function changePhoto2(fileName) {
+//     let img = document.querySelector("#bannerImage");
+//     img.setAttribute('src', fileName);
+//     section1.style.display = 'none'
+//     section2.style.display = 'block'
+//     section3.style.display = 'none'
+// }
+// function changePhoto1(fileName) {
+//     let img = document.querySelector("#bannerImage");
+//     img.setAttribute('src', fileName);
+//     section1.style.display = 'none'
+//     section2.style.display = 'none'
+//     section3.style.display = 'block'
+// }
+// function changePhoto3(fileName) {
+//     let img = document.querySelector("#bannerImage");
+//     img.setAttribute('src', fileName);
+//     section1.style.display = 'none'
+//     section2.style.display = 'block'
+//     section3.style.display = 'none'
+// }
+// function changePhoto1(fileName) {
+//     let img = document.querySelector("#bannerImage");
+//     img.setAttribute('src', fileName);
+//     section1.style.display = 'block'
+//     section2.style.display = 'none'
+//     section3.style.display = 'none'
+// }
